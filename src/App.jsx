d138a0d5c05dc1a8203beb3fea595c1fa13d4b4a
uzzam-afroz <uzzam.afroz@inaqani.com>
2024-11-12 16:24:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-red-600">
+        Hello from Design Synthesis React App with Tailwind
+      </h1>
+    </>
+  );
+}
+
+export default App;
